4e7febf2087a118a728eed72dfe02371cbc5160a
Artem Mikheev <30644072+renbou@users.noreply.github.com>
2024-10-06 00:23:39 +0300

    feat: landing page small-screen portrait layout

--- app/page.tsx
+++ app/page.tsx
@@ -46,27 +46,20 @@ function Home() {
   const navShake = [useState(false), useState(false), useState(false)];
 
   return (
-    <main className="w-screen h-screen px-[4%] 2xl:px-[8%] py-8">
-      <div
-        className="grid gap-x-8 gap-y-6"
-        style={{
-          gridTemplateColumns: "auto auto 1fr",
-        }}
-      >
+    <div className="md:w-screen md:h-screen px-[4%] 2xl:px-[8%] py-8">
+      <header className="grid header-grid gap-x-8 gap-y-6">
         {/* Branding */}
         <h1
           // justify-self-center for centering the top horizontal NEPLOX text to the width of the logo,
           // which is bigger horizontally than it is vertically
-          className="row-start-1 col-start-2 text-theme font-horizon justify-self-center leading-none"
-          style={{ fontSize: "min(10vh, 6vw)" }}
+          className="row-start-1 col-start-2 text-theme text-brand font-horizon justify-self-center leading-none"
         >
           NEPLOX
         </h1>
         <h1
-          className="row-start-2 col-start-1 text-theme font-horizon-outlined tracking-tighter leading-none"
+          className="row-start-2 col-start-1 text-theme text-brand font-horizon-outlined tracking-tighter leading-none"
           style={{
             writingMode: "vertical-lr",
-            fontSize: "min(10vh, 6vw)",
             WebkitTextStrokeWidth: "0.025em",
           }}
         >
@@ -74,22 +67,31 @@ function Home() {
         </h1>
         <Image
           // Extend vertically to the height of the sideways NEPLOX text, and automatically scale horizontally with the aspect ratio
-          className="row-start-2 col-start-2 lg:h-full w-auto select-none"
+          className="row-start-2 col-start-2 lg:h-full max-w-60 md:max-w-full w-auto mx-auto select-none"
           src="/icons/neplox.svg"
           alt="Neplox Logo"
           width={4122}
           height={3501}
         />
 
+        {/* Signature */}
+        <div className="row-start-3 col-start-1 -col-end-1 md:col-start-2 md:col-end-auto">
+          <h3 className="font-sans font-light text-center lg:text-lg h-full w-0 min-w-full max-w-0">
+            <span className="align-baseline hyphens-manual">
+              EST. 2024 BY CYBER&shy;SECURITY RESEARCHERS
+            </span>
+          </h3>
+        </div>
+
         {/* Nav */}
-        <div className="row-start-1 col-start-3">
+        <div className="row-start-4 col-start-1 -col-end-1 md:row-start-1 md:col-start-3 md:col-end-auto my-3 md:my-0">
           <nav className="max-w-lg h-full flex gap-x-4 justify-between mx-auto">
             {nav.map((text, index) => (
               <button
                 key={text}
-                className={`${canHover && "hover:animate-shake"} ${
-                  navShake[index][0] && "animate-shake text-shadow"
-                } font-sans font-normal text-nav lg:text-xl xl:text-2xl whitespace-nowrap cursor-not-allowed`}
+                className={`${canHover ? "hover:animate-shake" : ""} ${
+                  navShake[index][0] ? "animate-shake text-shadow" : ""
+                } font-sans font-normal text-xl text-nav lg:text-xl xl:text-2xl whitespace-nowrap cursor-not-allowed`}
                 // Avoid conflicting with the hover animation
                 onClick={() => !canHover && navShake[index][1](true)}
                 onAnimationEnd={() => navShake[index][1](false)}
@@ -104,26 +106,29 @@ function Home() {
         </div>
 
         {/* About */}
-        <div className="row-start-2 col-start-3 min-h-0 flex flex-col justify-evenly gap-y-6 items-center">
+        <div className="row-start-5 col-start-1 -col-end-1 md:row-start-2 md:col-start-3 md:-col-end-1 min-h-0 flex flex-col justify-evenly gap-y-6 items-center">
           <p className="font-normal font-serif lg:text-lg xl:text-xl text-justify leading-relaxed max-w-lg">
-            Formed by like-minded, top-tier <b>security researchers</b> from
-            diverse backgrounds, the <b className="text-theme">Neplox</b> team
-            is fueled by <b>curiosity</b> to explore and <b>secure</b> modern
-            systems.
+            Formed by like-minded, top-tier{" "}
+            <b className="qwqoro-test">security researchers</b> from diverse
+            backgrounds, the <b className="text-theme">Neplox</b> team is fueled
+            by <b className="qwqoro-test">curiosity</b> to explore and{" "}
+            <b className="qwqoro-test">secure</b> modern systems.
           </p>
           <p className="font-normal font-serif lg:text-lg xl:text-xl text-justify leading-relaxed max-w-lg">
             From international CTF winners to hardened reverse engineers and bug
             bounty hunters, our unique skillsets come together to offer a{" "}
-            <b>fresh perspective</b> on the security of <b>Web3</b> ecosystems.
+            <b className="qwqoro-test">fresh perspective</b> on the security of{" "}
+            <b className="qwqoro-test">Web3</b> ecosystems.
           </p>
         </div>
 
         {/* Socials */}
-        <div className="row-start-3 col-start-3 lg:row-start-2 lg:col-start-4 flex lg:flex-col justify-evenly lg:justify-around items-center">
+        <div className="row-start-2 col-start-3 md:row-start-3 md:col-start-3 md:col-end-auto lg:row-start-2 lg:col-start-4 flex flex-col md:flex-row lg:flex-col justify-evenly lg:justify-around items-center">
           {socials.map((social) => (
             <a
               key={social.href}
               href={social.href}
+              // w-8 here is important, it is equal to 2rem, which is also set as the font-size of the sideways NEPLOX text
               className="inline-block w-8 xl:w-12 h-auto hover:scale-125 transition-transform duration-300"
             >
               <Image
@@ -135,17 +140,8 @@ function Home() {
             </a>
           ))}
         </div>
-
-        {/* Footer */}
-        <div className="row-start-3 col-start-2">
-          <h3 className="font-sans font-light text-center lg:text-lg h-full w-0 min-w-full max-w-0">
-            <span className="align-baseline hyphens-manual">
-              EST. 2024 BY CYBER&shy;SECURITY RESEARCHERS
-            </span>
-          </h3>
-        </div>
-      </div>
-    </main>
+      </header>
+    </div>
   );
 }
 
