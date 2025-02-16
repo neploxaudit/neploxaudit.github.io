b0e8f3db84d833ae7f801c92ae3d3b0aacbd47b5
Artem Mikheev <30644072+renbou@users.noreply.github.com>
2025-02-16 18:11:17 +0300

    article title shaking for mobile devices

--- app/(articles)/ArticlePreview.tsx
+++ app/(articles)/ArticlePreview.tsx
@@ -1,6 +1,9 @@
+"use client";
+
+import Context from "@/app/components/Context";
 import Link from "next/link";
+import { useContext, useState } from "react";
 import { BiLinkExternal, BiSolidLock } from "react-icons/bi";
-import PreviewCard from "./PreviewCard";
 
 export type Article = {
   image: string;
@@ -34,22 +37,49 @@ export default function ArticlePreview({
   metadata,
   hidden,
 }: Article) {
+  const { canHover } = useContext(Context);
+  const [shaking, setShaking] = useState(false);
+  const classShaking = shaking ? "animate-shake" : "";
+
   return (
     <div
       className={`article-preview ${hidden && "article-hidden"} transition-transform duration-300 hover:scale-105 hover:cursor-not-allowed`}
+      onClick={(e) => {
+        if (!hidden) {
+          return;
+        }
+        if (!canHover) {
+          setShaking(true);
+        }
+        e.preventDefault();
+      }}
     >
       <MaybeLink href={metadata.href ?? ""}>
-        <PreviewCard
-          image={image}
-          hidden={hidden}
-          title={title}
-          metadata={metadata}
-        />
+        <div
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundColor: "oklch(from var(--element) l c h / 0.2)",
+          }}
+          className={`article-preview-card flex aspect-[16/10] h-auto w-full flex-col items-end justify-between rounded-xl object-cover px-6 py-4 transition select-none ${hidden && "opacity-85 grayscale-32"}`}
+          title={hidden ? undefined : title}
+        >
+          <span className={`text-xs ${hidden && "opacity-40"}`}>
+            [ {metadata.date} ]
+          </span>
+          <span className={`text-xs ${hidden && "opacity-40"}`}>
+            by [ {metadata.author} ]
+          </span>
+        </div>
         <h2
           className={`mt-4 mb-2 flex justify-between font-theme-sans text-xl transition-all duration-300 ${hidden && "break-words opacity-35"}`}
           style={{ WebkitTextStrokeWidth: "0.01em" }}
         >
-          <span className="article-preview-title mx-3">{title}</span>
+          <span
+            className={`article-preview-title mx-3 ${classShaking}`}
+            onAnimationEnd={() => hidden && !canHover && setShaking(false)}
+          >
+            {title}
+          </span>
           <span className="mx-5 mt-1 font-light">
             {hidden ? <BiSolidLock /> : <BiLinkExternal />}
           </span>
