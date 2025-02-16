c59b8e37ff8dae8d9bf99683d21031f9e49dab54
Artem Mikheev <30644072+renbou@users.noreply.github.com>
2025-02-17 00:38:57 +0300

    finish responsive footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,19 @@
 import Socials from "@/app/components/Socials";
+import Link from "next/link";
+import { BiCopyright } from "react-icons/bi";
+import ScrollUp from "./ScrollUp";
 import Signature from "./Signature";
+
+const footerLinks = [
+  { name: "about us", href: "/" },
+  { name: "ctf", href: "/ctf" },
+  { name: "extensions", href: "https://extensions.neplox.security" },
+];
 
 export default function Footer({ className }: { className?: string }) {
   return (
-    <footer className={`${className} lg:mt-4`}>
-      <div className="mx-auto mb-4 flex w-3/4 flex-row items-center justify-evenly md:w-1/3">
+    <footer className={`${className} mt-12 lg:mt-16`}>
+      <div className="mx-auto flex w-3/4 items-center justify-evenly md:w-1/3">
         {Socials.map((Social) => (
           <a
             key={Social.href}
@@ -15,7 +24,26 @@ export default function Footer({ className }: { className?: string }) {
           </a>
         ))}
       </div>
-      <Signature className="text-xs sm:text-sm lg:text-base xl:text-lg" />
+      <div className="mx-auto my-8 flex w-4/5 justify-center gap-x-8 px-2 lg:gap-x-12">
+        {footerLinks.map((link) => (
+          <Link
+            className="text-base whitespace-nowrap transition-transform duration-200 hover:scale-110 md:text-lg"
+            key={link.href}
+            href={link.href}
+          >
+            {link.name.toUpperCase()}
+          </Link>
+        ))}
+      </div>
+      <Signature className="text-sm sm:text-base lg:text-lg" />
+      <hr className="mx-auto my-4 h-px w-4/5 border-0 bg-element opacity-15" />
+      <div className="mx-auto flex w-4/5 items-center justify-between px-2">
+        <span className="text-center text-sm font-light opacity-70 sm:text-base lg:text-lg">
+          Neplox Team <wbr /> <BiCopyright className="inline" /> 2024 &ndash;{" "}
+          {new Date().getFullYear()}
+        </span>
+        <ScrollUp />
+      </div>
     </footer>
   );
 }
