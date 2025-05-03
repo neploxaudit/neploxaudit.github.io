917fe8960ea25fc7c26f612087077a992e28172f
Artem Mikheev <30644072+renbou@users.noreply.github.com>
2025-05-03 12:21:51 +0300

    wip: image with alt mdx component

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,19 @@
 import type { MDXComponents } from "mdx/types";
 import Image from "next/image";
+import React from "react";
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    img: Image,
+    img: (props: React.ComponentProps<typeof Image>) => {
+      return (
+        <>
+          <Image {...props} />
+          <span className="text-center text-sm text-stone-500">
+            {props.alt}
+          </span>
+        </>
+      );
+    },
     ...components,
   };
 }
