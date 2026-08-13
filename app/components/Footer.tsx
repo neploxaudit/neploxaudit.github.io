e6bed829525f5589ff178ca67f6071392acce725
Elizabeth <96961942+qwqoro@users.noreply.github.com>
2026-08-13 21:22:15 +0200

    edit footer information

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -42,17 +42,16 @@ export default function Footer({ className }: { className?: string }) {
       </div>
       <div className="mx-auto my-4 w-4/5 text-justify font-theme-sans font-light text-xs text-raisin-500 sm:text-sm lg:text-base dark:text-stone-500">
         <p className="mx-4 align-baseline hyphens-manual">NEPLOX LTD<br/>
-        Registered in England and Wales.<br/>Company No. <code>17399310</code><br/>
+        Registered in England and Wales.<br/>Company No. <code className="font-theme-mono">17399310</code><br/>
         Registered Office: 4th Floor Office, 205 Regent Street, London, England, W1B 3HH</p>
       </div>
       <hr className="mx-auto my-4 h-px w-4/5 border-0 bg-element opacity-15" />
       <div className="mx-auto flex w-4/5 items-center justify-between px-2">
         <div className="text-center font-light lg:flex lg:flex-row lg:items-center">
           <span className="text-sm text-raisin-500 sm:text-base lg:text-lg dark:text-stone-500">
-            <span className="whitespace-nowrap">Neplox Team</span>{" "}
+            <span className="whitespace-nowrap">Neplox Ltd.</span>{" "}
             <span className="whitespace-nowrap">
-              <BiCopyright className="inline" /> 2024 &ndash;{" "}
-              {new Date().getFullYear()}
+              <BiCopyright className="inline" /> {new Date().getFullYear()}
             </span>
           </span>
           <a
